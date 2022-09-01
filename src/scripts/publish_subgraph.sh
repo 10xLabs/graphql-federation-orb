04fc1202e4139adbcbcd02fc14913c631fcc1d2f
jbpineiroc <jbpineiroc@gmail.com>
2022-09-01 11:18:56 -0500

    fix: add -api suffix to subdomain name

--- src/scripts/publish_subgraph.sh
+++ src/scripts/publish_subgraph.sh
@@ -3,4 +3,4 @@ find "$DIRECTORY" -name "*.graphql" -exec cat {} \; > schema.graphql
 
 rover subgraph publish "$SUPERGRAPH@$ENVIRONMENT" \
     --name "$CIRCLE_PROJECT_REPONAME" --schema schema.graphql \
-    --routing-url "https://$CIRCLE_PROJECT_REPONAME.$DOMAIN_NAME/graphql"
+    --routing-url "https://$CIRCLE_PROJECT_REPONAME-api.$DOMAIN_NAME/graphql"
