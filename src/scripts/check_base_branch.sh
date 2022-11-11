b717fe84cc796a8f7234a354f4d3e1bab675ebf5
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2022-11-11 10:22:23 -0600

    fix: disable check

--- src/scripts/check_base_branch.sh
+++ src/scripts/check_base_branch.sh
@@ -7,6 +7,7 @@ fi
 pull_request=$(echo "https://api.github.com/repos/${CIRCLE_PULL_REQUEST:19}" | sed "s/\/pull\//\/pulls\//")
 base_branch=$(curl -s -H "Authorization: token ${GITHUB_PAT}" "$pull_request" | jq -r '.base.ref')
 
+# shellcheck disable=SC2153
 if [ "$base_branch" != "$BASE_BRANCH" ]; then
     circleci-agent step halt
     exit 0
