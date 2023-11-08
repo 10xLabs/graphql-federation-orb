395806b7f354f0c4776c19c53a3832e2da80cd91
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2023-11-08 09:32:01 -0600

    chore: skip base branch check if empty

--- src/scripts/check_base_branch.sh
+++ src/scripts/check_base_branch.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-if [ -z "$CIRCLE_PULL_REQUEST" ]; then
-    circleci-agent step halt
-    exit 0
-fi
+if [ -n "$CIRCLE_PULL_REQUEST" ] && [ -n "$BASE_BRANCH" ]; then
+    pull_request=$(echo "https://api.github.com/repos/${CIRCLE_PULL_REQUEST:19}" | sed "s/\/pull\//\/pulls\//")
+    base_branch=$(curl -s -H "Authorization: token ${GITHUB_PAT}" "$pull_request" | jq -r '.base.ref')
 
-pull_request=$(echo "https://api.github.com/repos/${CIRCLE_PULL_REQUEST:19}" | sed "s/\/pull\//\/pulls\//")
-base_branch=$(curl -s -H "Authorization: token ${GITHUB_PAT}" "$pull_request" | jq -r '.base.ref')
-
-# shellcheck disable=SC2153
-if [ "$base_branch" != "$BASE_BRANCH" ]; then
-    circleci-agent step halt
-    exit 0
+    # shellcheck disable=SC2153
+    if [ "$base_branch" != "$BASE_BRANCH" ]; then
+        circleci-agent step halt
+        exit 0
+    fi
 fi
