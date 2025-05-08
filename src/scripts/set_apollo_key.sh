b43af58a5309cb8cea352088daa2e440312d49dc
blablo <blablo@gmail.com>
2025-05-08 11:44:08 -0400

    fix: supergraph

--- src/scripts/set_apollo_key.sh
+++ src/scripts/set_apollo_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2153
-supergraph="${SUPERGRAPH:0:27}@$ENVIRONMENT"
+supergraph="${SUPERGRAPH:0:27}"
 APOLLO_KEY_NAME="$(echo "$supergraph" | tr '-' '_' | tr '[:lower:]' '[:upper:]')_APOLLO_KEY"
 
 echo "export APOLLO_KEY=${!APOLLO_KEY_NAME}" >>"$BASH_ENV"
