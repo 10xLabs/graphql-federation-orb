8b6b86557508d54767a17cb08c5e32c91dc9b4f4
blablo <blablo@gmail.com>
2025-05-08 12:57:07 -0400

    fix: routing url

--- src/scripts/publish_subgraph.sh
+++ src/scripts/publish_subgraph.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2153
 subgraph="$CIRCLE_PROJECT_REPONAME"
 supergraph="${SUPERGRAPH:0:27}@$ENVIRONMENT"
-routing_url="https://${SUPERGRAPH//@router/@gateway}.$DOMAIN_NAME/subgraph/$subgraph/graphql"
+routing_url="https://${SUPERGRAPH//router/gateway}.$DOMAIN_NAME/subgraph/$subgraph/graphql" # Corrected substitution
 echo "Subgraph: $subgraph" 
 echo "Supergraph: $supergraph" 
 echo "Routing URL: $routing_url"
@@ -12,4 +12,3 @@ rover subgraph publish "$supergraph" \
     --schema schema.graphql \
     --routing-url "$routing_url"
     # --routing-url "https://$CIRCLE_PROJECT_REPONAME-$SUBGRAPH-api.$DOMAIN_NAME/graphql"
-
